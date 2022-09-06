7daaeab0b040f8f8c2c1cd6ff2000f5197f947cc
jochemwhite <34311247+jochemwhite@users.noreply.github.com>
2022-09-06 16:34:35 +0200

    Update mysql.js

--- API/mysql.js
+++ API/mysql.js
@@ -3,7 +3,7 @@ import mysql from "mysql2";
 const con = mysql.createConnection({
   host: "157.90.198.147",
   user: "Jochem",
-  password: "Maxie2001@",
+  password: "",
   database: "ROUTER",
 });
 
